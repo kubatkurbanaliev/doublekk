1994d9fa8647fd101506d0c736f4d1aa17881441
kubatkurbanaliev <kurbanalievkubat@gmail.com>
2019-12-16 15:08:50 -0600

    Added new system.out

--- src/test/java/GitTestPackage/GitTestCalss.java
+++ src/test/java/GitTestPackage/GitTestCalss.java
@@ -3,5 +3,7 @@ package GitTestPackage;
 public class GitTestCalss {
     public static void main(String[] args) {
         System.out.println("New Class");
+
+        System.out.println("Hi Kira");
     }
 }
